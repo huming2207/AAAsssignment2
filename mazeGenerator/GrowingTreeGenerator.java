bde441ff410a21d1ddf07c19a096989196520e98
Jackson Ming Hu <huming2207@gmail.com>
2017-09-19 22:08:20 +1000

    Generator: implement DFS

--- mazeGenerator/GrowingTreeGenerator.java
+++ mazeGenerator/GrowingTreeGenerator.java
@@ -18,4 +18,6 @@ public class GrowingTreeGenerator implements MazeGenerator
 
     }
 
+
+
 }
--- mazeGenerator/RecursiveBacktrackerGenerator.java
+++ mazeGenerator/RecursiveBacktrackerGenerator.java
@@ -1,6 +1,11 @@
 package mazeGenerator;
 
+import maze.Cell;
 import maze.Maze;
+import maze.Wall;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RecursiveBacktrackerGenerator implements MazeGenerator
 {
@@ -8,8 +13,63 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator
     @Override
     public void generateMaze(Maze maze)
     {
-        // TODO Auto-generated method stub
-
+        initDfs(maze);
     } // end of generateMaze()
+
+
+    /**
+     *
+     * DFS initiator
+     *
+     * @param maze The maze
+     */
+    private void initDfs(Maze maze)
+    {
+        // Declare a DFS marked list and marked the initial values as false.
+        boolean[][] markedList = new boolean[maze.sizeR][maze.sizeC];
+
+        // Fire the hole!
+        runDfs(maze.entrance, markedList);
+    }
+
+    /**
+     * Perform DFS and traverse the whole map from a certain root cell
+     *
+     * @param rootShell The root shell
+     * @param markedList The marked list bool array
+     */
+    private void runDfs(Cell rootShell, boolean[][] markedList)
+    {
+        // Mark the list and rebuild the wall
+        markedList[rootShell.r][rootShell.c] = true;
+        rebuildWall(rootShell);
+
+        // Iterate the neighbor shell
+        for(Cell currentCell : rootShell.neigh)
+        {
+            // Detect if it has been marked, if it does, skip to the next one
+            if(!markedList[currentCell.r][currentCell.c])
+            {
+                runDfs(currentCell, markedList);
+            }
+        }
+
+    }
+
+    /**
+     *
+     *  Build the new wall!
+     *
+     * @param cell The cell need to be rebuild the wall
+     */
+    private void rebuildWall(Cell cell)
+    {
+        // Grab a random index
+        int randomIndex = ThreadLocalRandom.current().nextInt(0, cell.wall.length);
+
+        // Set the wall present value to false
+        cell.wall[randomIndex].present = false;
+    }
+
 
 } // end of class RecursiveBacktrackerGenerator
