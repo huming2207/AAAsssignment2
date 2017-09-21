b369c49740ba17005ea40e97999a667cbe330954
Jackson Ming Hu <huming2207@gmail.com>
2017-09-21 10:54:27 +1000

    Generator: Now DFS works as expected!

--- mazeGenerator/RecursiveBacktrackerGenerator.java
+++ mazeGenerator/RecursiveBacktrackerGenerator.java
@@ -4,7 +4,9 @@ import maze.Cell;
 import maze.Maze;
 import maze.Wall;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RecursiveBacktrackerGenerator implements MazeGenerator
@@ -28,64 +30,67 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator
         // Declare a DFS marked list and marked the initial values as false.
         boolean[][] markedList = new boolean[maze.sizeR][maze.sizeC];
 
-        for(boolean[] array : markedList)
-        {
-            Arrays.fill(array, false);
-        }
+        // Randomly pick a Cell from the maze map.
+        int randomPosX = ThreadLocalRandom.current().nextInt(0, maze.sizeC);
+        int randomPosY = ThreadLocalRandom.current().nextInt(0, maze.sizeR);
+        Cell randomCell = maze.map[randomPosX][randomPosY];
 
-        // Fire the hole!
-        runDfs(maze.entrance, markedList);
+        // Fire in the hole!
+        runDfs(randomCell, markedList);
     }
 
     /**
      * Perform DFS and traverse the whole map from a certain root cell
      *
-     * @param rootShell The root shell
+     * @param rootCell The root cell
      * @param markedList The marked list bool array
      */
-    private void runDfs(Cell rootShell, boolean[][] markedList)
+    private void runDfs(Cell rootCell, boolean[][] markedList)
     {
         // Mark the list and rebuild the wall
-        markedList[rootShell.r][rootShell.c] = true;
-        rebuildWall(rootShell);
+        markedList[rootCell.r][rootCell.c] = true;
+
+        // Randomize the cells
+        ArrayList<Cell> shuffledCell = new ArrayList<>();
+        Collections.addAll(shuffledCell, rootCell.neigh.clone());
+        Collections.shuffle(shuffledCell);
 
         // Iterate the neighbor shell
-        for(Cell currentCell : rootShell.neigh)
+        for(Cell currentCell : shuffledCell)
         {
             // Detect if it has been marked, if it does, skip to the next one
             if(currentCell != null && !markedList[currentCell.r][currentCell.c])
             {
-                runDfs(currentCell, markedList);
+                rebuildWall(rootCell, currentCell);
+                runDfs(currentCell,  markedList);
             }
         }
-
     }
 
     /**
      *
      *  Build the new wall!
      *
-     * @param cell The cell need to be rebuild the wall
+     * @param cell The root cell need to be rebuild the wall
      */
-    private void rebuildWall(Cell cell)
+    private void rebuildWall(Cell cell, Cell neighborCell)
     {
-        // Grab a random index
-        int randomIndex = ThreadLocalRandom.current().nextInt(0, cell.wall.length);
-
-        for(Wall wall : cell.wall)
+        for(int cellIndex = 0; cellIndex <= 5; cellIndex++)
         {
-            if(wall != null)
+            if(neighborCell.neigh[cellIndex] != null
+                    && cell.c == neighborCell.neigh[cellIndex].c
+                    && cell.r == neighborCell.neigh[cellIndex].r)
             {
-                // If the wall is null, pick again.
-                if(cell.wall[randomIndex] != null)
-                {
-                    // Set the wall present value to false
-                    cell.wall[randomIndex].present = false;
-                }
+                neighborCell.wall[cellIndex].present = false;
+            }
+
+            if(cell.neigh[cellIndex] != null
+                    && cell.neigh[cellIndex].c == neighborCell.c
+                    && cell.neigh[cellIndex].r == neighborCell.r)
+            {
+                cell.wall[cellIndex].present = false;
             }
         }
 
     }
-
-
 } // end of class RecursiveBacktrackerGenerator
